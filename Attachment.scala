4ca54996b4f211adf917ffb7147b2c87f9b0ca3d
AlexanderBengtBrodin <144924504+AlexanderBengtBrodin@users.noreply.github.com>
2025-02-07 09:12:59 +0100

    Overhauled main class to be more direct

--- Attachment.scala
+++ Attachment.scala
@@ -7,13 +7,7 @@ enum Attachment(modifier: (SymbolTally) => SymbolTally):
         return modifier.apply(sym)
 
     case balancedHilt extends Attachment(a => a.addSymbol(Symbol.Surge,1))
+    case tacticalDisplay extends Attachment(a => a.addSymbol(Symbol.Surge,1))
     case smartHilt extends Attachment(a => if a(Symbol.Surge) == 0 then a.addSymbol(Symbol.Surge,1) else a )
     case autoCleave2 extends Attachment((a) => a.addSymbol(Symbol.Damage,2))
     case plus1Damage extends Attachment((a) => a.addSymbol(Symbol.Damage,1))
-
-
-
-// class Attachment(modifier: (SymbolTally) => SymbolTally) {
-//     def modify(tally: SymbolTally): SymbolTally =
-//         modifier.apply(tally)
-// }
---
+++ TestMain.scala
@@ -0,0 +1,64 @@
+package ImAs
+
+import scala.compiletime.ops.double
+import ImAs.Weapons
+import ImAs.Defenders
+import Dice.* 
+import ImAs.Weapons.IfRifle
+
+object TestMain {
+
+    def main(args: Array[String]): Unit = 
+        def test(weapon: Weapon, defender: Defender, amount: Int): Float =
+            var freq: SymbolTally = SymbolTally()
+
+            for i <- 1 to amount do
+                freq = freq + weapon.attack(defender)
+
+            println("Average Damage: " + s"${freq(Symbol.Damage).toFloat/amount}")
+            println("Average Strain Recovered: " + s"${-freq(Symbol.Strain).toFloat/amount}")
+
+            return freq(Symbol.Damage).toFloat/amount
+
+        def testDice(dicePool: Vector[Dice], amount: Int): Vector[Float] =
+            var freq: SymbolTally = SymbolTally()
+
+            for i <- 1 to amount do
+                freq = freq + dicePool.map(_.roll()).reduce(_ + _) 
+
+            return Vector(freq(Symbol.Damage).toFloat/amount, freq(Symbol.Surge).toFloat/amount)
+
+        def printingTest(weaponName: String, weapon: Weapon, defender: Defender, amount: Int): Float =
+            println("Testing " + weaponName + ":")
+            val output = test(weapon, defender ,amount)
+            println("")
+            output
+
+
+        //Example probability tests
+
+        // printingTest(
+        //     "IfRifle", 
+        //     Weapons.IfRifle, 
+        //     Defenders.evadingShyla, 
+        //     1000000
+        //     )
+
+        //Weapons.IfRifle.Attachments = Vector(Attachment.tacticalDisplay) //Override
+
+        // printingTest(
+        //     "Tactical Display IfRifle", 
+        //     Weapons.IfRifle, 
+        //     Defenders.evadingShyla, 
+        //     1000000
+        //     )
+
+        
+        // printingTest(
+        //     "Lightsaber",
+        //     Weapons.lightSaber,
+        //     Defenders.blackDice,
+        //     1000000
+        // )
+        
+}
